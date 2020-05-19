e0bb2fa3e255b74f73eb147791e1c995e1f0e4f2
Isha Patro <isha.patro.215@gmail.com>
2020-05-20 03:06:05 +0530

    Appointment page with Department Filte

---
+++ js/script.js
@@ -0,0 +1,51 @@
+$(window).scroll(function ()
+
+    {
+        if ( $(window).scrollTop() > 50)
+        {
+            $("#NavBar").addClass("fixed-top");
+        }
+        else  if ($(window).scrollTop() < 50)
+        {
+            $("#NavBar").removeClass("fixed-top");
+        }
+    });
+
+// ------------------------------------------------------------------Department Filtering----------------------------------------------------------------------
+// Show filtered elements
+function AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+function ChangeDepartment() {
+  var x, i, c, department;
+  department = document.getElementById("department");
+  c = department.options[department.selectedIndex].value;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+  }
+}
