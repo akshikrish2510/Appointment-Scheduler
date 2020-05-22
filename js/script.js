396c6b632ed99cf0e44ed31fe79b368044a72db5
Isha Patro <isha.patro.215@gmail.com>
2020-05-23 02:43:25 +0530

    Home Page

--- js/script.js
+++ js/script.js
@@ -42,8 +42,6 @@ function ChangeDepartment() {
   department = document.getElementById("department");
   c = department.options[department.selectedIndex].value;
   x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
     RemoveClass(x[i], "show");
     if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
